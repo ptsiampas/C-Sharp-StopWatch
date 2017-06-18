4494d4a59a1c24c4bdd87febaabafbd2edd021d2
Peter Tsiampas <peter@tsiampas.com>
2017-06-18 18:12:20 +1000

    Add project files.

---
+++ c_sharp_intermediate_exercises/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace c_sharp_intermediate_exercises
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var stopWatch = new StopWatch();
+            int milliseconds = 2000;
+
+            stopWatch.Start();
+            Thread.Sleep(milliseconds);
+            stopWatch.Stop();
+
+            Console.WriteLine("Elapsed Time: " + stopWatch.ElapsedTime);
+
+            stopWatch.Start();
+            Thread.Sleep(milliseconds);
+            stopWatch.Stop();
+
+            Console.WriteLine("Elapsed Time: " + stopWatch.ElapsedTime);
+
+            // Will throw exception because we tried to start it twice.
+            stopWatch.Start();
+            stopWatch.Start();
+
+        }
+    }
+}
